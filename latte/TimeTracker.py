72a23bb8c995d8e959a83b02c04db6c57bf9e879
Tautvidas <t.9b5as@gmail.com>
2012-06-02 22:43:19 +0300

    Fixing for pylint

--- latte/TimeTracker.py
+++ latte/TimeTracker.py
@@ -51,15 +51,16 @@ class TimeTracker:
         self.logs = {}
 
     def dumpLogs(self):
-        target_path = os.path.join(self._configs['appPath'], self._configs['statsPath'])
+        target_path = os.path.join(self._configs['appPath'],
+                                   self._configs['statsPath'])
         if not os.path.exists(target_path):
             os.makedirs(target_path)
         filename = str(int(time.time()))
         file_path = os.path.join(target_path, filename)
 
-        file = open(file_path, 'w')
-        file.write(json.dumps(self.getLogs(), indent=4))
-        file.close()
+        log_file = open(file_path, 'w')
+        log_file.write(json.dumps(self.getLogs(), indent=4))
+        log_file.close()
 
         self.clearLogs()
         return filename
