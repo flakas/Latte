180d181a0faef0ddbe5b3e230844a63a16e2be1d
Tautvidas <t.9b5as@gmail.com>
2012-07-07 15:15:13 +0200

    Modifying installation

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ except ImportError:
 config = {
     'description': 'Automatic rule based Linux Time Tracker',
     'author': 'Tautvidas Sipavicius',
-    'url': 'https://github.com/flakas/Latte',
+    'homepage': 'https://github.com/flakas/Latte',
+    'url' : 'https://github.com/flakas/Latte/tarball/latte-1.0',
     'install_requires': [],
     'packages': ['latte', 'latte.Categories', 'latte.Projects'],
     'scripts': ['bin/latte'],
