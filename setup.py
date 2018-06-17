16c6e009a36203256507d89d790af3d94be7fdb5
Tautvidas Sipavičius <t.9b5as@gmail.com>
2018-06-18 00:20:44 +0300

    Add a clearer explanation and categorization for Pypi in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 config = {
     'description': 'Automatic rule based Linux Time Tracker',
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
     'author': 'Tautvidas Sipavicius',
     'author_email': 'flakas@tautvidas.com',
     'url': 'https://github.com/flakas/Latte',
@@ -12,7 +17,12 @@ config = {
     'packages': ['latte', 'latte.os'],
     'scripts': ['bin/latte', 'bin/lattestats'],
     'version': '4.0',
-    'name': 'latte'
+    'name': 'latte',
+    'classifiers': (
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+    ),
 }
 
 setup(**config)
